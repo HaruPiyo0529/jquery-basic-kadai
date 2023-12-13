9e42c56e6b8cb8512064970318694b318825ab77
HaruPiyo0529 <146844973+HaruPiyo0529@users.noreply.github.com>
2023-12-13 18:14:13 +0900

    kadai_004をアップロード

---
+++ kadai_004/event.js
@@ -0,0 +1,15 @@
+$(window).on(
+    // ロードしたら
+    'load', function() {
+        //コンソール出力
+        console.log('loadイベントが発生しました');  
+    });
+
+$(window).on(
+// スクロールしたら
+'scroll', function() {
+    // コンソール出力
+    console.log('scrollイベントが発生しました');
+    });
+
+    
